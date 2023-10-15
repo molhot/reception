dfa96002ef24c88f83402b3eede01b923fc11304
molhot <sakata19991214@gmail.com>
2023-10-15 21:34:13 +0900

    push

--- srcs/requirements/wordpress/tools/configure.sh
+++ srcs/requirements/wordpress/tools/configure.sh
@@ -31,6 +31,9 @@ else
     >&2 echo "Install already WordPress"
 fi
 chown -R www-data:www-data /var/www/html/* && find /var/www/html/ -type d -exec chmod 755 {} + && find /var/www/html/ -type f -exec chmod 644 {} +
+sed -i "s|listen = /run/php/php7.3-fpm.sock|listen = 9000|g" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s|skip-networking|# skip-networking|g" /etc/php/7.3/fpm/pool.d/www.conf
+sed -i "s|;request_terminate_timeout = 0|request_terminate_timeout = 300|g" /etc/php/7.3/fpm/pool.d/www.conf
 # PIDファイル
 mkdir -p /run/php
 echo "start php-fpm7.3"
